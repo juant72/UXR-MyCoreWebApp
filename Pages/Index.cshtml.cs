b4229e275d325541a46a71b19a611e246f1b0b6a
Angelos Petropoulos <angelpe@microsoft.com>
2022-03-07 11:21:35 -0600

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyCoreWebApp.Data;
+
+namespace MyCoreWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            dbContext.Database.Migrate();
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
